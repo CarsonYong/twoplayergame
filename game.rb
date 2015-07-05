f830c9270ab24f7b25a0b5a62749b150c3fdade2
Carson Yong <CarsonYong@gmail.com>
2015-07-05 18:59:58 -0400

    Added some comments

--- game.rb
+++ game.rb
@@ -1,3 +1,4 @@
+#Array that stores a hash of the players info.
 @players = [
 	{
 		id: 1,
@@ -17,36 +18,35 @@ def generate_question(players)
 	turn = 0
   counter = 0 
 	while players[turn][:life] != 0 #|| players[1][:life] != 0 
-	 num1 = rand(1..20)
-	 num2 = rand(1..20)
-	 turn = counter % 2
+	   num1 = rand(1..20)
+	   num2 = rand(1..20)
+	   turn = counter % 2  # If divisible is 0 turn is player 1, if divisible is 1 it is player 2's turn.
     #if turn == 0 
     # puts "turn 0"
     #else puts "turn 1"
     #end
 		 
-	 print "Player #{players[turn][:id]}: what is #{num1} + #{num2}? "
-	 answer = num1 + num2
-	 response = gets.chomp.to_i
-    counter += 1
+	   print "Player #{players[turn][:id]}: what is #{num1} + #{num2}? "
+	   answer = num1 + num2
+	   response = gets.chomp.to_i
+      counter += 1
 	
-	 if response == answer 
-	   players[turn][:score] += 1
-      #puts  "The score is #{players[turn][:score]}"
-    elsif response != answer
-      players[turn][:life] -= 1
-      puts "That was the wrong answer. The score is: \n Player 1: #{players[0][:score]} \n Player 2: #{players[1][:score]}"
-	 end
-  #if players[turn][:life]
-  end
-  if players[0][:life] == 1
-    puts "Player # 1 won with a score of #{players[0][:score]}"
-    puts "Player # 2 lost with a score was #{players[1][:score]}"
-  else
-    puts "Player # 2 won with a score of #{players[1][:score]}"
-    puts "Player # 1 lost with a score of #{players[1][:score]}" 
-end
-  
+	     if response == answer 
+	     players[turn][:score] += 1
+        #puts  "The score is #{players[turn][:score]}"
+      elsif response != answer
+        players[turn][:life] -= 1
+        puts "That was the wrong answer. The score is: \n Player 1: #{players[0][:score]} \n Player 2: #{players[1][:score]}"
+	     end
+    #if players[turn][:life]
+    end
+    if players[0][:life] == 1
+      puts "Player # 1 won with a score of #{players[0][:score]}"
+      puts "Player # 2 lost with a score was #{players[1][:score]}"
+    else
+      puts "Player # 2 won with a score of #{players[1][:score]}"
+      puts "Player # 1 lost with a score of #{players[1][:score]}" 
+    end  
 end
 
 
